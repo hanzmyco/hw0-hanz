22885ff8ad288305263ab47f7c95619971d47615
‘michaelzhhan1 <hanz@cs.cmu.edu>
2014-09-07 14:35:22 -0400

    last

---
+++ hw0-hanz/src/main/java/DependencyExample.java
@@ -0,0 +1,18 @@
+import java.io.StringReader;
+import edu.stanford.nlp.ling.Word;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.process.PTBTokenizer.PTBTokenizerFactory;
+import edu.stanford.nlp.process.Tokenizer;
+/**
+ * An example for Homework 0 of 11791 F13
+ *
+ * @author Zi Yang <ziy@cs.cmu.edu>
+*/
+public class DependencyExample {
+
+
+  public static void main(String[] args) {
+    TokenizerFactory<Word> factory =PTBTokenizerFactory.newTokenizerFactory();
+    Tokenizer<Word> tokenizer =factory.getTokenizer(new StringReader(args[0]));
+    System.out.println(tokenizer.tokenize());
+} }
